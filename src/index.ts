c479210e96e4614b2fb8798741c2271da2126068
Matt Oestreich <matthewpoestreich@gmail.com>
2025-06-18 14:13:31 -0500

    updates

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,9 @@
 import { V8RandomnessPredictor, FirefoxRandomnessPredictor, ChromeRandomnessPredictor } from "./predictors/index.js";
 
-export default {
-  firefox(sequence: number[]) {
-    return new FirefoxRandomnessPredictor(sequence);
-  },
-  chrome(sequence: number[]) {
-    return new ChromeRandomnessPredictor(sequence);
-  },
-  v8(sequence?: number[]) {
-    return new V8RandomnessPredictor(sequence);
-  },
+const JSRandomnessPredictor = {
+  v8: (sequence?: number[]) => new V8RandomnessPredictor(sequence),
+  firefox: (sequence: number[]) => new FirefoxRandomnessPredictor(sequence),
+  chrome: (sequence: number[]) => new ChromeRandomnessPredictor(sequence),
 };
+
+export default JSRandomnessPredictor;
