0de55f6a98b77ad5f5bb9244bd1ba7059242882c
Matt Oestreich <matthewpoestreich@gmail.com>
2025-09-23 10:38:41 -0500

    Update testVersionsWithoutNodeTest.cjs

--- tests/node/scripts/testVersionsWithoutNodeTest.cjs
+++ tests/node/scripts/testVersionsWithoutNodeTest.cjs
@@ -10,6 +10,7 @@ const JsRandomnessPredictor = require("../../../dist/cjs/index.js");
 const INITIAL_SEQUENCE_LEN = 4;
 const EXPECTED_PREDICTIONS_LEN = 10;
 const NODE_MAJOR_VERSION = Number(process.versions.node.split(".")[0]);
+const EXIT = { code: 0 };
 
 const hashTags = (n = 1) => "#".repeat(n);
 
@@ -18,16 +19,18 @@ console.log(`Tests using auto-generated sequence.`);
 console.log(`This script exists because 'node:test' does not exist in v17-v19.`);
 console.log(`Meant for Node.js versions 17-19 | Current version=${NODE_MAJOR_VERSION}\n${hashTags(100)}`);
 
-if (NODE_MAJOR_VERSION > 19) {
-  console.log(`[SKIPPING] Current Node Version > 19, making this test redundant.\n${hashTags(100)}`);
-  process.exit(0);
-} else {
-  Main();
+if (NODE_MAJOR_VERSION > 19 || NODE_MAJOR_VERSION < 17) {
+  const condition = NODE_MAJOR_VERSION < 17 ? "< 17" : "> 19";
+  console.log(`[SKIPPING] Current Node Version is ${condition}, making this test redundant.\n${hashTags(100)}`);
+  process.exit(EXIT.code);
 }
 
+// Invoke script/main method.
+Main();
+
 async function Main() {
-  const seq = Array.from({ length: INITIAL_SEQUENCE_LEN }, Math.random);
-  const predictor = JsRandomnessPredictor.node(seq);
+  const sequence = Array.from({ length: INITIAL_SEQUENCE_LEN }, Math.random);
+  const predictor = JsRandomnessPredictor.node(sequence);
   const expected = Array.from({ length: EXPECTED_PREDICTIONS_LEN }, Math.random);
 
   const predictions = [];
@@ -36,11 +39,14 @@ async function Main() {
     predictions.push(prediction);
   }
 
-  if (expected.every((v, i) => predictions[i] === v)) {
-    console.log({ predictions, expected, result: `[SUCCESS] Predictions are accurate.` });
-    process.exit(0);
-  } else {
-    console.log({ predictions, expected, result: `[FAILURE] Predictions not accurate.` });
-    process.exit(1);
+  // Assume success by default.
+  const output = { sequence, predictions, expected, result: `[SUCCESS] Predictions are accurate.` };
+
+  if (!expected.every((v, i) => predictions[i] === v)) {
+    output.result = `[FAILURE] Predictions not accurate.`;
+    EXIT.code = 1;
   }
+
+  console.log(output);
+  process.exit(EXIT.code);
 }
