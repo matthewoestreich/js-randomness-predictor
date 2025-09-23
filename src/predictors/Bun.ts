b59ed1eae03ea43d32d3fc24ac04fffb94437f6d
Matt Oestreich <matthewpoestreich@gmail.com>
2025-09-23 10:08:02 -0500

    Update Bun.ts

--- src/predictors/Bun.ts
+++ src/predictors/Bun.ts
@@ -1,7 +1,5 @@
 import SafariRandomnessPredictor from "./Safari.js";
 
-// CANNOT USE BUN REPL TO GENERATE RANDOM NUMBERS!!
-
 // Both Bun and Safari use JavaScriptCore engine.
 // The Bun runtime doesn't appear to do anything special, therefore we can
 // extend the SafariRandomnessPredictor for now.
