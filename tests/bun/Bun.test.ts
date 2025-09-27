dd57961748fbbbe88711ee5fc19fadf6c5dcc176
Matt Oestreich <matthewpoestreich@gmail.com>
2025-09-27 11:59:43 -0500

    Update Bun.test.ts

--- tests/bun/Bun.test.ts
+++ tests/bun/Bun.test.ts
@@ -1,4 +1,4 @@
-import { describe, it, expect } from "bun:test";
+import { describe, it, expect, TestOptions } from "bun:test";
 import BunRandomnessPredictor from "../../src/predictors/Bun.ts";
 
 function callMathRandom(nTimes = 1): number[] {
@@ -9,58 +9,76 @@ function callMathRandom(nTimes = 1): number[] {
   return o;
 }
 
+const testOptions: TestOptions = { timeout: 15000, retry: 3 };
+
 describe("Bun", () => {
-  it("'sequence' generated with Array.from(), 'expected' generated with Math.random()", async () => {
-    /**
-     * If/when this test starts failing, it means the bug in JavaScriptCore has been patched!
-     */
-    const sequence = Array.from({ length: 8 }, Math.random);
-    const expected = callMathRandom(10);
-    const predictor = new BunRandomnessPredictor(sequence);
-    const predictions: number[] = [];
-    for (let i = 0; i < expected.length; i++) {
-      predictions.push(await predictor.predictNext());
-    }
-    // ~ Due to current JavaScriptCore bug ~
-    // Array.from({length:n},Math.random) takes the slow baseline path.
-    // Math.random() takes the JIT path.
-    // Since they took two diff paths, we should expect their results to not be equal.
-    expect(predictions).not.toStrictEqual(expected);
-  });
+  it(
+    "'sequence' generated with Array.from(), 'expected' generated with Math.random()",
+    async () => {
+      /**
+       * If/when this test starts failing, it means the bug in JavaScriptCore has been patched!
+       */
+      const sequence = Array.from({ length: 7 }, Math.random);
+      const expected = callMathRandom(6);
+      const predictor = new BunRandomnessPredictor(sequence);
+      const predictions: number[] = [];
+      for (let i = 0; i < expected.length; i++) {
+        predictions.push(await predictor.predictNext());
+      }
+      // ~ Due to current JavaScriptCore bug ~
+      // Array.from({length:n},Math.random) takes the slow baseline path.
+      // Math.random() takes the JIT path.
+      // Since they took two diff paths, we should expect their results to not be equal.
+      expect(predictions).not.toStrictEqual(expected);
+    },
+    testOptions,
+  );
 
-  it("both 'sequence' and 'expected' generated with Array.from()", async () => {
-    const sequence = Array.from({ length: 10 }, Math.random);
-    const expected = Array.from({ length: 10 }, Math.random);
-    const predictor = new BunRandomnessPredictor(sequence);
-    const predictions: number[] = [];
-    for (let i = 0; i < expected.length; i++) {
-      predictions.push(await predictor.predictNext());
-    }
-    // Our sequence and expected both generated via Array.from, so
-    // they should have taken the same path, thus making them equal.
-    expect(predictions).toStrictEqual(expected);
-  });
+  it(
+    "both 'sequence' and 'expected' generated with Array.from()",
+    async () => {
+      const sequence = Array.from({ length: 7 }, Math.random);
+      const expected = Array.from({ length: 6 }, Math.random);
+      const predictor = new BunRandomnessPredictor(sequence);
+      const predictions: number[] = [];
+      for (let i = 0; i < expected.length; i++) {
+        predictions.push(await predictor.predictNext());
+      }
+      // Our sequence and expected both generated via Array.from, so
+      // they should have taken the same path, thus making them equal.
+      expect(predictions).toStrictEqual(expected);
+    },
+    testOptions,
+  );
 
-  it("both 'sequence' and 'expected' generated with Math.random()", async () => {
-    const sequence = callMathRandom(8);
-    const expected = callMathRandom(10);
-    const predictor = new BunRandomnessPredictor(sequence);
-    const predictions: number[] = [];
-    for (let i = 0; i < expected.length; i++) {
-      predictions.push(await predictor.predictNext());
-    }
-    // Our sequence and expected both generated via Math.random calls, so
-    // they should have taken the same path, thus making them equal.
-    expect(predictions).toStrictEqual(expected);
-  });
+  it(
+    "both 'sequence' and 'expected' generated with Math.random()",
+    async () => {
+      const sequence = callMathRandom(7);
+      const expected = callMathRandom(6);
+      const predictor = new BunRandomnessPredictor(sequence);
+      const predictions: number[] = [];
+      for (let i = 0; i < expected.length; i++) {
+        predictions.push(await predictor.predictNext());
+      }
+      // Our sequence and expected both generated via Math.random calls, so
+      // they should have taken the same path, thus making them equal.
+      expect(predictions).toStrictEqual(expected);
+    },
+    testOptions,
+  );
 
-  it("tests with a dynamically generated sequence", async () => {
-    const predictor = new BunRandomnessPredictor();
-    const expected = callMathRandom(10);
-    const predictions: number[] = [];
-    for (let i = 0; i < expected.length; i++) {
-      predictions.push(await predictor.predictNext());
-    }
-    expect(predictions).toStrictEqual(expected);
-  });
+  it(
+    "tests with a dynamically generated sequence",
+    async () => {
+      const predictor = new BunRandomnessPredictor();
+      const expected = callMathRandom(6);
+      const predictions: number[] = [];
+      for (let i = 0; i < expected.length; i++) {
+        predictions.push(await predictor.predictNext());
+      }
+      expect(predictions).toStrictEqual(expected);
+    },
+    testOptions,
+  );
 });
