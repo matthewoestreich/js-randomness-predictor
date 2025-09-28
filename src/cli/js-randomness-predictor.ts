7f8491b9a9fb6207759293815ea79142206e5f14
Matt Oestreich <matthewpoestreich@gmail.com>
2025-09-28 15:27:00 -0500

    fix test and package.json

--- src/cli/js-randomness-predictor.ts
+++ src/cli/js-randomness-predictor.ts
@@ -6,6 +6,8 @@ import { runPredictor } from "./runPredictor.js";
 import { PredictorArgs, PredictorResult, RUNTIMES, NODE_MAJOR_VERSIONS, IS_SERVER_RUNTIME } from "../types.js";
 import { SequenceNotFoundError } from "../errors.js";
 import Logger from "../logger.js";
+import { getCurrentNodeJsMajorVersion } from "./utils.js";
+import ExecutionRuntime from "../ExecutionRuntime.js";
 
 const predictCommand: CommandModule = {
   command: "*",
@@ -62,25 +64,23 @@ const predictCommand: CommandModule = {
         type: "boolean",
       })
       .check((argv) => {
-        const isNode = argv.environment === "node";
-        const isNodeVersionMatch = argv.envVersion === argv._currentNodeJsMajorVersion;
-        const isSequenceRequired = !IS_SERVER_RUNTIME[argv.environment];
-
         // If the --environment is not a server runtime the --sequence is required!
-        if (!argv.sequence && isSequenceRequired) {
+        if (!argv.sequence && !IS_SERVER_RUNTIME[argv.environment]) {
           throw new SequenceNotFoundError(`'--sequence' is required when '--environment' is '${argv.environment}'`);
         }
 
-        // * If:
-        //    - The --environment is node
-        //    - The --env-version was provided
-        //    - The --sequence was NOT provided
-        //    - The --env-version does NOT match the users current running node version
-        // * Then:
-        //    Throw an error. Sequence is required when doing `-e node -v X` where `X` is != current nodejs major version.
-        if (isNode && argv.envVersion && !argv.sequence && !isNodeVersionMatch) {
+        // If we are running in Node and user provided "-e node" as well as "--env-version N" without "--sequence",
+        // but the current Node execution version doesn't match with "--env-version N", it means we can't generate
+        // a reliable sequence, so the user HAS to provide a "--sequence" argument. Let them know about this error.
+        if (
+          ExecutionRuntime.isNode() &&
+          argv.environment === "node" &&
+          argv.envVersion &&
+          !argv.sequence &&
+          argv.envVersion !== getCurrentNodeJsMajorVersion()
+        ) {
           throw new SequenceNotFoundError(
-            `'--sequence' is required when '--environment' is '${argv.environment}' and '--env-version' is different than your current Node.js version!`,
+            `'--sequence' is required when '--environment' is '${argv.environment}' and '--env-version' is different than your current Node.js version! Current Node version is '${getCurrentNodeJsMajorVersion()}' but --env-version is '${argv.envVersion}'`,
           );
         }
 
