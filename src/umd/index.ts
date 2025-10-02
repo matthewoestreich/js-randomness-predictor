fc6de55178778d0465bedb724e27cacc9d50d893
Matt Oestreich <matthewpoestreich@gmail.com>
2025-10-02 18:30:16 -0500

    [publish] #patch

--- src/umd/index.ts
+++ src/umd/index.ts
@@ -1,11 +1,7 @@
-import { firefox, safari, chrome } from "../index.js";
+import * as jsrp from "../index.js";
 import loader from "./loader.js";
 
 // Invoke immediately upon page load.
 loader();
 
-export default {
-  firefox,
-  safari,
-  chrome,
-};
+export default { ...jsrp };
