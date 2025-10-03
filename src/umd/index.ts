3fcf9d1cfe405c2069ce5d9758f0e8f492b48d7f
Matt Oestreich <matthewpoestreich@gmail.com>
2025-10-02 21:16:28 -0500

    remove tests/browser, add helper fn

--- src/umd/index.ts
+++ src/umd/index.ts
@@ -1,7 +1,22 @@
+import ExecutionRuntime from "../ExecutionRuntime.js";
 import JSRandomnessPredictor from "../index.js";
+import { BrowserRuntimeType, Predictor, SUPPORTED_BROWSER_RUNTIMES } from "../types.js";
 import loader from "./loader.js";
 
 // Invoke immediately upon page load.
 loader();
 
 export default JSRandomnessPredictor;
+
+export function getCurrentBrowser(): BrowserRuntimeType | undefined {
+  if (ExecutionRuntime.isChrome()) {
+    return "chrome";
+  }
+  if (ExecutionRuntime.isSafari()) {
+    return "safari";
+  }
+  if (ExecutionRuntime.isFirefox()) {
+    return "firefox";
+  }
+  return undefined;
+}
