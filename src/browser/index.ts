0b4d399008cd403aa659f8fde31511f73c98cdcc
Matt Oestreich <matthewpoestreich@gmail.com>
2025-10-04 12:42:41 -0500

    [publish] #patch

---
+++ src/browser/index.ts
@@ -0,0 +1,46 @@
+import getCurrentBrowser from "./getCurrentBrowser.js";
+import loader from "./loader.js";
+import {
+  FirefoxRandomnessPredictor,
+  ChromeRandomnessPredictor,
+  NodeRandomnessPredictor,
+  SafariRandomnessPredictor,
+  BunRandomnessPredictor,
+  DenoRandomnessPredictor,
+} from "../public_types.js";
+
+import type { BrowserRuntimeType } from "../types.js";
+import type { Predictor, SemanticVersion } from "../public_types.js";
+
+// Invoke immediately upon page load.
+loader();
+
+export type { BrowserRuntimeType, Predictor, SemanticVersion };
+
+const node = (sequence?: number[]): NodeRandomnessPredictor => new NodeRandomnessPredictor(sequence);
+const firefox = (sequence: number[]): FirefoxRandomnessPredictor => new FirefoxRandomnessPredictor(sequence);
+const chrome = (sequence: number[]): ChromeRandomnessPredictor => new ChromeRandomnessPredictor(sequence);
+const safari = (sequence: number[]): SafariRandomnessPredictor => new SafariRandomnessPredictor(sequence);
+const bun = (sequence?: number[]): BunRandomnessPredictor => new BunRandomnessPredictor(sequence);
+const deno = (sequence?: number[]): DenoRandomnessPredictor => new DenoRandomnessPredictor(sequence);
+
+/********** For CJS browser builds *************/
+const JSRandomnessPredictor = { node, firefox, chrome, safari, bun, deno };
+exports = JSRandomnessPredictor;
+/***********************************************/
+
+export {
+  FirefoxRandomnessPredictor,
+  ChromeRandomnessPredictor,
+  NodeRandomnessPredictor,
+  SafariRandomnessPredictor,
+  BunRandomnessPredictor,
+  DenoRandomnessPredictor,
+  node,
+  firefox,
+  chrome,
+  safari,
+  bun,
+  deno,
+  getCurrentBrowser,
+};
--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,51 @@
-import { defineConfig } from "vite";
+import { BuildEnvironmentOptions, defineConfig } from "vite";
 import nodepath from "node:path";
+
+type BuildTargets = "umd" | "cjs";
+type BuildTargetsMap = Record<BuildTargets, boolean>;
+
+const BUILD_TARGETS: BuildTargetsMap = { cjs: true, umd: true };
+const BUILD_TARGET: BuildTargets | undefined = process.env.BUILD_TARGET as BuildTargets;
+
+if (!BUILD_TARGET || BUILD_TARGET === undefined) {
+  throw new Error(`[vite] process.env.BUILD_TARGET is not defined! Please use one of "${Object.keys(BUILD_TARGETS)}"`);
+}
+if (!BUILD_TARGETS[BUILD_TARGET]) {
+  throw new Error(`[vite] Unknown build target in 'process.env.BUILD_TARGET'. Expected one of '${Object.keys(BUILD_TARGETS)}' got '${BUILD_TARGET}'`);
+}
+
+// Default to umd.
+const BUILD: BuildEnvironmentOptions = {
+  lib: {
+    entry: nodepath.resolve(__dirname, "./src/browser/index.ts"),
+    name: "JSRandomnessPredictor",
+    formats: ["umd"],
+    fileName: () => "js-randomness-predictor.js",
+  },
+  outDir: "dist/umd",
+};
+
+if (BUILD_TARGET === "cjs") {
+  BUILD.lib = {
+    ...BUILD.lib,
+    entry: nodepath.resolve(__dirname, "./src/browser/index.ts"),
+    formats: ["cjs"],
+    fileName: () => "index.js",
+  };
+  BUILD.outDir = "dist/browser";
+}
 
 export default defineConfig({
   resolve: {
     alias: {
       // Alias these so our browser shim works correctly.
-      "z3-solver-jsrp": "./src/umd/shim.ts",
+      "z3-solver-jsrp": "./src/browser/shim.ts",
       "z3-solver-jsrp-low-level": "z3-solver-jsrp/dist/build/browser/low-level",
       "z3-solver-jsrp-high-level": "z3-solver-jsrp/dist/build/browser/high-level",
     },
   },
   build: {
-    lib: {
-      entry: nodepath.resolve(__dirname, "./src/umd/index.ts"),
-      name: "JSRandomnessPredictor",
-      formats: ["umd"],
-      fileName: () => "js-randomness-predictor.js",
-    },
+    ...BUILD,
     minify: true,
-    outDir: "dist/umd",
-    emptyOutDir: true,
   },
 });
