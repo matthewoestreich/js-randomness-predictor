c9045598824f86249fcf4e27deef3515f278cb81
Matt Oestreich <matthewpoestreich@gmail.com>
2025-10-03 20:32:38 -0500

    fix test

--- tests/import/cjs.test.cjs
+++ tests/import/cjs.test.cjs
@@ -4,7 +4,6 @@ const assert = require("node:assert");
 const JSRandomnessPredictor = require("../../dist/cjs/cjs.js");
 
 test("CJS build can be required", () => {
-  JSRandomnessPredictor.bun();
   assert.ok(JSRandomnessPredictor, "CJS require should return something");
   assert.equal(typeof JSRandomnessPredictor.node, "function");
 });
