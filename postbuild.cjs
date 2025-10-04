93ce1f078f4eab2209c6497a01942b927df5e6f7
Matt Oestreich <matthewpoestreich@gmail.com>
2025-10-04 13:14:56 -0500

    [publish] #patch

--- postbuild.cjs
+++ postbuild.cjs
@@ -1,11 +1,11 @@
 #!/usr/bin/env node
+const nodefs = require("node:fs");
+const nodepath = require("node:path");
 
-const nodeFs = require("node:fs");
-const nodePath = require("node:path");
-
-createPackageJsonInCjsDist(nodePath.resolve(__dirname, "./dist/cjs"));
-createPackageJsonInUmdDist(nodePath.resolve(__dirname, "./dist/umd"));
-copyImportMapToBuild(nodePath.resolve(__dirname, "./src/cli/deno_import_map.json"));
+createPackageJsonInCjsDist(nodepath.resolve(__dirname, "./dist/cjs"));
+createPackageJsonInUmdDist(nodepath.resolve(__dirname, "./dist/umd"));
+createPackageJsonInBrowserCjsDist(nodepath.resolve(__dirname, "./dist/browser"));
+copyImportMapToBuild(nodepath.resolve(__dirname, "./src/cli/deno_import_map.json"));
 
 /**
  * For the cjs build
@@ -15,11 +15,15 @@ function createPackageJsonInCjsDist(distPath = "") {
   if (distPath === "") {
     throw new Error("[postbuild.js][create package.json in CJS dist] distPath required!");
   }
-  nodeFs.mkdirSync(distPath, { recursive: true });
-  const pkgJson = { type: "commonjs", types: "../types/index.d.ts" };
-  const pkgJsonString = JSON.stringify(pkgJson, null, 2);
-  const outFile = nodePath.resolve(__dirname, distPath, "package.json");
-  nodeFs.writeFileSync(outFile, pkgJsonString);
+  try {
+    nodefs.mkdirSync(distPath, { recursive: true });
+    const pkgJson = { type: "commonjs", types: "../types/index.d.ts" };
+    const pkgJsonString = JSON.stringify(pkgJson, null, 2);
+    const outFile = nodepath.resolve(__dirname, distPath, "package.json");
+    nodefs.writeFileSync(outFile, pkgJsonString);
+  } catch (e) {
+    throw new Error(`[postbuild.js][create package.json in CJS dist] GOT ERROR : ${e.message}`);
+  }
 }
 
 /**
@@ -30,11 +34,34 @@ function createPackageJsonInUmdDist(distPath = "") {
   if (distPath === "") {
     throw new Error("[postbuild.js][create package.json in UMD dist] distPath required!");
   }
-  nodeFs.mkdirSync(distPath, { recursive: true });
-  const pkgJson = { types: "../types/umd/index.d.ts" };
-  const pkgJsonString = JSON.stringify(pkgJson, null, 2);
-  const outFile = nodePath.resolve(__dirname, distPath, "package.json");
-  nodeFs.writeFileSync(outFile, pkgJsonString);
+  try {
+    nodefs.mkdirSync(distPath, { recursive: true });
+    const pkgJson = { types: "../types/umd/index.d.ts" };
+    const pkgJsonString = JSON.stringify(pkgJson, null, 2);
+    const outFile = nodepath.resolve(__dirname, distPath, "package.json");
+    nodefs.writeFileSync(outFile, pkgJsonString);
+  } catch (e) {
+    throw new Error(`[postbuild.js][create package.json in UMD dist] GOT ERROR : ${e.message}`);
+  }
+}
+
+/**
+ * For the browser/cjs build
+ * @param {string} distPath : path to dist folder
+ */
+function createPackageJsonInBrowserCjsDist(distPath = "") {
+  if (distPath === "") {
+    throw new Error("[postbuild.js][create package.json in browser/cjs dist] distPath required!");
+  }
+  try {
+    nodefs.mkdirSync(distPath, { recursive: true });
+    const pkgJson = { type: "commonjs", types: "../types/index.d.ts" };
+    const pkgJsonString = JSON.stringify(pkgJson, null, 2);
+    const outFile = nodepath.resolve(__dirname, distPath, "package.json");
+    nodefs.writeFileSync(outFile, pkgJsonString);
+  } catch (e) {
+    throw new Error(`[postbuild.js][create package.json in browser/cjs dist] GOT ERROR : ${e.message}`);
+  }
 }
 
 /**
@@ -45,6 +72,10 @@ function copyImportMapToBuild(importMapPath = "") {
   if (importMapPath === "") {
     throw new Error("[postbuild.js][copy import map for CLI build] need import map for deno");
   }
-  const copyToPath = nodePath.resolve(__dirname, "./dist/esm/cli/deno_import_map.json");
-  nodeFs.copyFileSync(importMapPath, copyToPath);
+  try {
+    const copyToPath = nodepath.resolve(__dirname, "./dist/esm/cli/deno_import_map.json");
+    nodefs.copyFileSync(importMapPath, copyToPath);
+  } catch (e) {
+    throw new Error(`[postbuild.js][copy import map for CLI build] GOT ERROR : ${e.message}`);
+  }
 }
