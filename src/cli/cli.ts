0b6c8f96d3163b40eda4d433e3667ca40c7d89de
Matt Oestreich <matthewpoestreich@gmail.com>
2025-10-06 19:10:17 -0500

    [publish] #patch

--- src/cli/cli.ts
+++ src/cli/cli.ts
@@ -11,7 +11,6 @@
 
 import { spawnSync, SpawnSyncOptionsWithBufferEncoding } from "node:child_process";
 import nodepath from "node:path";
-import nodefs from "node:fs";
 import { EXECUTION_RUNTIME_ENV_VAR_KEY } from "../types.js";
 
 // Relative paths.
@@ -25,9 +24,13 @@ const js_randomness_predictor = nodepath.resolve(import.meta.dirname, jsrpRelati
 // These are the args that a user provided to js-randomness-predictor CLI.
 const jsrpArgs = process.argv.slice(2);
 // These are the args for the child process we are about to run.
-const finalArgs = [js_randomness_predictor, ...jsrpArgs];
+const childProcessArgs = [js_randomness_predictor, ...jsrpArgs];
 // Options for child process we are about to run.
-const childProcessOptions: SpawnSyncOptionsWithBufferEncoding = { stdio: "inherit", env: { ...process.env } };
+const childProcessOptions: SpawnSyncOptionsWithBufferEncoding = {
+  stdio: "inherit",
+  env: { ...process.env },
+  shell: process.platform === "win32",
+};
 
 // Deno is so special!
 if (executionRuntime === "deno") {
@@ -36,10 +39,10 @@ if (executionRuntime === "deno") {
 
   // Deno forces us to put the "--allow-*" commands PRIOR to the script!
   // So the command ultimately becomes:`deno --allow-env --allow-read js-randomness-predictor.js <rest_of_cli_args>`
-  finalArgs.unshift("--allow-env", "--allow-read", `--import-map=${importMap}`);
+  childProcessArgs.unshift("--allow-env", "--allow-read", `--import-map=${importMap}`);
 
   // So we can use imports that arent prefixed with "npm:", eg `import x from "npm:x"`
   childProcessOptions.env!.DENO_COMPAT = "1";
 }
 
-spawnSync(executionRuntime.toString(), finalArgs, childProcessOptions);
+spawnSync(executionRuntime.toString(), childProcessArgs, childProcessOptions);
