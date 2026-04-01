d956e925c81afd198c06f8b3d463d7ea5da78d4d
Matt Oestreich <matthewpoestreich@gmail.com>
2026-04-01 01:57:50 -0500

    refactor(cli): if execution runtime is node, there is no need to spawn child process

--- src/cli/cli.ts
+++ src/cli/cli.ts
@@ -4,19 +4,18 @@ import { spawnSync, SpawnSyncOptionsWithBufferEncoding } from "node:child_proces
 import nodepath from "node:path";
 import { EXECUTION_RUNTIME_ENV_VAR_KEY } from "../constants.js";
 
-// Relative path(s).
-const denoImportMapRelative = "./deno_import_map.json";
-const jsrpRelative = "./js-randomness-predictor.js";
-// Node is our default execution runtime.
 const executionRuntime = process.env[EXECUTION_RUNTIME_ENV_VAR_KEY]?.trim() || "node";
-const js_randomness_predictor = nodepath.resolve(import.meta.dirname, jsrpRelative);
-// These are the args that a user provided to js-randomness-predictor CLI.
-const jsrpArgs = process.argv.slice(2);
-// This is essentially argv for the CLI.
-const args = [js_randomness_predictor, ...jsrpArgs];
 
-// Need child process for runtimes other than Node.
-if (executionRuntime !== "node") {
+// No need for child process if we are using Node execution runtime, since we are already in Node.
+if (executionRuntime === "node") {
+  // File MUST be imported at runtime, otherwise this won't work!!
+  import("./js-randomness-predictor.js").then(({ default: buildCli }) => buildCli().parse()).catch(() => process.exit(1));
+} else {
+  /**
+   * Need child process for runtimes other than Node.
+   **/
+
+  const argv = [nodepath.resolve(import.meta.dirname, "./js-randomness-predictor.js"), ...process.argv.slice(2)];
   const childProcessOptions: SpawnSyncOptionsWithBufferEncoding = {
     stdio: "inherit",
     env: { ...process.env },
@@ -25,19 +24,15 @@ if (executionRuntime !== "node") {
 
   if (executionRuntime === "deno") {
     // Deno needs an import map...smh
-    const importMap = nodepath.resolve(import.meta.dirname, denoImportMapRelative);
+    const importMap = nodepath.resolve(import.meta.dirname, "./deno_import_map.json");
     // Deno forces us to put the "--allow-*" commands PRIOR to the script!
     // So the command ultimately becomes:`deno --node-modules-dir --allow-env --allow-read js-randomness-predictor.js <rest_of_cli_args>`
-    args.unshift("--node-modules-dir=auto", "--allow-env", "--allow-read", `--import-map=${importMap}`);
+    argv.unshift("--node-modules-dir=auto", "--allow-env", "--allow-read", `--import-map=${importMap}`);
     // So we can use imports that arent prefixed with "npm:", eg `import x from "npm:x"`
     childProcessOptions.env!.DENO_COMPAT = "1";
   }
 
-  const result = spawnSync(`${executionRuntime.toString()} ${args.join(" ")}`, childProcessOptions);
+  const result = spawnSync(`${executionRuntime.toString()} ${argv.join(" ")}`, childProcessOptions);
   // Bubble up process status
   process.exit(result.status ?? 1);
 }
-
-// No need for child process if we are using Node execution runtime, since we are already in Node.
-// File MUST be imported at runtime, otherwise this won't work!!
-import("./js-randomness-predictor.js").then(({ default: buildCli }) => buildCli().parse(args)).catch(() => process.exit(1));
