9297d4b18b7d267fb1e3ce95f106fb52d1ecf036
Matt Oestreich <matthewpoestreich@gmail.com>
2026-06-20 10:17:14 -0500

    test(node): troubleshoot flaky test

--- tests/node/Node.test.ts
+++ tests/node/Node.test.ts
@@ -11,14 +11,16 @@ describe("Node", () => {
   });
 
   it("predict correctly when using Array.from for 'sequence' and 'expected'", async () => {
-    const sequence = Array.from({ length: 4 }, Math.random);
-    const nodePredictor = JSRandomnessPredictor.node(sequence);
-    const expected = Array.from({ length: 5 }, Math.random);
-    const predictions: number[] = [];
-    for (let i = 0; i < expected.length; i++) {
-      predictions.push(await nodePredictor.predictNext());
-    }
-    assert.deepStrictEqual(expected, predictions);
+    await withRetries(async () => {
+      const sequence = Array.from({ length: 4 }, Math.random);
+      const nodePredictor = JSRandomnessPredictor.node(sequence);
+      const expected = Array.from({ length: 5 }, Math.random);
+      const predictions: number[] = [];
+      for (let i = 0; i < expected.length; i++) {
+        predictions.push(await nodePredictor.predictNext());
+      }
+      assert.deepStrictEqual(expected, predictions);
+    }, 3);
   });
 
   it("predict correctly when using single Math.random() calls for 'sequence' and 'expected'", async () => {
@@ -46,33 +48,37 @@ describe("Node", () => {
   });
 
   it("predict correctly when using dynamic 'sequence' and Array.from for 'expected'", async () => {
-    const nodePredictor = JSRandomnessPredictor.node();
-    const expected = Array.from({ length: 10 }, Math.random);
-    const predictions: number[] = [];
-    for (let i = 0; i < expected.length; i++) {
-      predictions.push(await nodePredictor.predictNext());
-    }
-    assert.deepStrictEqual(expected, predictions);
+    await withRetries(async () => {
+      const nodePredictor = JSRandomnessPredictor.node();
+      const expected = Array.from({ length: 10 }, Math.random);
+      const predictions: number[] = [];
+      for (let i = 0; i < expected.length; i++) {
+        predictions.push(await nodePredictor.predictNext());
+      }
+      assert.deepStrictEqual(expected, predictions);
+    }, 3);
   });
 
   it("predict correctly when using dynamic 'sequence' and single Math.random() calls for 'expected'", async () => {
-    const nodePredictor = JSRandomnessPredictor.node();
-    const expected = [
-      Math.random(),
-      Math.random(),
-      Math.random(),
-      Math.random(),
-      Math.random(),
-      Math.random(),
-      Math.random(),
-      Math.random(),
-      Math.random(),
-      Math.random(),
-    ];
-    const predictions: number[] = [];
-    for (let i = 0; i < expected.length; i++) {
-      predictions.push(await nodePredictor.predictNext());
-    }
-    assert.deepStrictEqual(expected, predictions);
+    await withRetries(async () => {
+      const nodePredictor = JSRandomnessPredictor.node();
+      const expected = [
+        Math.random(),
+        Math.random(),
+        Math.random(),
+        Math.random(),
+        Math.random(),
+        Math.random(),
+        Math.random(),
+        Math.random(),
+        Math.random(),
+        Math.random(),
+      ];
+      const predictions: number[] = [];
+      for (let i = 0; i < expected.length; i++) {
+        predictions.push(await nodePredictor.predictNext());
+      }
+      assert.deepStrictEqual(expected, predictions);
+    }, 3);
   });
 });
